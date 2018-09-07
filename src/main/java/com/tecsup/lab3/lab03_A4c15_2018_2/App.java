fb352997be18c018673f8b9ccbcc0ff79cc466bc
piero <piero_2103@hotmail.com>
2018-09-07 17:23:18 -0500

    primera parte

--- src/main/java/com/tecsup/lab3/lab03_A4c15_2018_2/App.java
+++ src/main/java/com/tecsup/lab3/lab03_A4c15_2018_2/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "hola    mundo  lab 03" );
+      
+        int i;
+        for(i=0;i<500;i++) 
+        {System.out.println("hola mundo"+i);}
+        
+        
     }
 }
